5cf8d3b622b4d82bb87352ce0332eacd95c7d54d
Huseyin0138 <huseyinbaskurt528@gmail.com>
2025-12-30 00:26:12 +0100

    Add player input fields and buttons for Okey Punkte Zähler game

---
+++ OkeyPunkteZähler/script.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var table = document.getElementById("punkteliste");
+    var row = table.insertRow(2);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = document.getElementById("Punkte").innerHTML;
+    cell2.innerHTML = "NEW CELL2";
+    var cell3 = row.insertCell(2);
+    cell3.innerHTML = "NEW CELL3";
+    var cell4 = row.insertCell(3);
+    cell4.innerHTML = "NEW CELL4";
+    var cell5 = row.insertCell(4);
+    cell5.innerHTML = "NEW CELL5";
+}
+
+
+//das ist für Spieler 1
+function gösterge01(){
+
+}
+
+function normalbitis01(){
+    
+}
+
+function okeybitis01(){
+    
+}
